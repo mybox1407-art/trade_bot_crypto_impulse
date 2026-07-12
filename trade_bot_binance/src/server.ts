8d13c8ad2a207a1258096d317efecb9e4f06ad71
mybox1407-art <mybox1407@gmail.com>
2026-07-12 16:59:09 +0400

    Update server.ts

--- trade_bot_binance/src/server.ts
+++ trade_bot_binance/src/server.ts
@@ -1,10 +1,6 @@
-import dotenv from 'dotenv';
 import { app } from './app';
+import { env } from './config/env';
 
-dotenv.config();
-
-const port = process.env.PORT || 3001;
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(env.port, () => {
+  console.log(`Server started on port ${env.port}`);
 });
